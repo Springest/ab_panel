ac42a6785cdbfe4502bfed2e769bac17ea8c6c17
Mark Mulder <markmulder@gmail.com>
2013-10-10 10:50:23 +0200

    Correct controller_additions_spec
    The rename was done in 755d4a8ddbd17f42a2cfaa56fc80bb703767a5a5 but I 
    forgot to update the spec to reflect this as well.

--- spec/ab_panel/controller_additions_spec.rb
+++ spec/ab_panel/controller_additions_spec.rb
@@ -11,8 +11,8 @@ end
 describe AbPanel::ControllerAdditions do
   let(:controller) { Controller.new }
 
-  describe "#ab_panel_id" do
-    subject { controller.ab_panel_id }
+  describe "#distinct_id" do
+    subject { controller.distinct_id }
 
     it { should match /^([A-Z]|[0-9])([A-Z]|[0-9])([A-Z]|[0-9])([A-Z]|[0-9])([A-Z]|[0-9])$/ }
   end
