b93505af6359b1e118a2a990644cbc41076ad26b
Mark Mulder <markmulder@gmail.com>
2013-11-15 16:39:21 +0100

    Output tracking properties in AbPanel::Javascript

--- lib/ab_panel/javascript.rb
+++ lib/ab_panel/javascript.rb
@@ -1,11 +1,14 @@
 module AbPanel
   class Javascript
     def self.environment
-      props = { distinct_id: AbPanel.env["distinct_id"] }
+      props = {
+        distinct_id: AbPanel.env["distinct_id"]
+      }.merge(AbPanel.env[:properties])
 
       AbPanel.funnels.each { |f| props["funnel_#{f}"] = true }
 
       AbPanel.experiments.each { |exp| props[exp] = AbPanel.conditions.send(exp).condition }
+
       props.to_json
     end
   end
