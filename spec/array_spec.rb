6ca3dca838d684b85afb5b922edc3e9937485840
Peter de Ruijter <peter@l2code.nl>
2014-03-17 15:31:28 +0100

    More boundary tests for Array.

--- spec/array_spec.rb
+++ spec/array_spec.rb
@@ -13,11 +13,34 @@ describe Array do
 
     let(:array) { [1, 2, 3, 4] }
     subject { array.weighted_sample }
+
     it { should eq 3 }
+
+    context "different random" do
+      before do
+        Kernel.stub(:rand) { 0 }
+      end
+
+      it { should eq 1 }
+    end
+
+    context "different random" do
+      before do
+        Kernel.stub(:rand) { 1 }
+      end
+
+      it { should eq 4 }
+    end
 
     context "with weights" do
       subject { array.weighted_sample([1, 0, 0, 0]) }
       it { should eq 1 }
+    end
+    
+    context "all the same weights" do
+      before { Kernel.stub(:rand) { 1 } }
+      subject { array.weighted_sample([0, 0, 0, 0]) }
+      it { should eq 4 }
     end
   end
 end
