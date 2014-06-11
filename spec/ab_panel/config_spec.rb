66ef5dd2075f9ef55e4bbfbd73fc6ef738315ff9
Peter de Ruijter <peter@l2code.nl>
2014-06-11 15:08:05 +0200

    Fixes the specs so that it tests something.

--- spec/ab_panel/config_spec.rb
+++ spec/ab_panel/config_spec.rb
@@ -2,33 +2,29 @@ require 'spec_helper'
 
 describe AbPanel::Config do
   let(:config) { AbPanel::Config.new }
-  before do
-    AbPanel::Config.any_instance.stub(:settings) { { exp1: { scenario1: 25, scenario2: 75 } } }
+  context "config" do
+    before do
+      AbPanel::Config.any_instance.stub(:settings) { { exp1: { scenario1: 25, scenario2: 75 } } }
+    end
+
+    describe '.experiments' do
+      subject { config.experiments }
+      it { should =~ [:exp1] }
+    end
+
+    describe '.weights' do
+      subject { config.weights('exp1') }
+
+      it { should =~ [75.0, 25.0] }
+    end
   end
-
-  describe '.experiments' do
-    subject { config.experiments }
-    it { should =~ [:exp1] }
-  end
-
-  describe '.weights' do
-    subject { config.weights('exp1') }
-
-    it { should =~ [75.0, 25.0] }
-  end
-
   context "empty config" do
     before do
-      AbPanel::Config.any_instance.stub(:settings) { }
+      YAML.stub(:load) { false }
     end
     describe ".settings" do
       subject { config.settings }
       it { should eq nil }
-    end
-
-    describe ".experiments" do
-      subject { config.experiments }
-      it { should = {} }
     end
   end
 end
